55bd5a7236bad52d565d316415578b25a32eafca
Ashley Davis <ashley@codecapers.com.au>
2022-12-31 08:40:26 +1000

    Integrated the sidebar that was prototyped separately.

--- src/app.jsx
+++ src/app.jsx
@@ -5,25 +5,61 @@ import { UploadPage } from "./pages/upload";
 
 export class App extends React.Component {
 
+    constructor(properties) {
+        super(properties);
+
+        this.state = {
+            sidebarOpen: false,
+        };
+    }
+
     render() {
         return (
-            <BrowserRouter>
-                <div className="nav">
-                    <div className="title">Photosphere</div>
-                    <Link to="/" className="link">Home</Link>
-                    <Link to="/upload" className="link">Upload</Link>
+            <div className="host">
+                <div id="sidebar" className={this.state.sidebarOpen ? "open" : ""} >
+                    <h3>
+                        Photosphere
+                    </h3>
+
+                    <br />
+
+                    <h3>Storage</h3>
+                    <h3>Local</h3>
+                    <h3>Upload</h3>
+                    <h3>Favorites</h3>
+                    <h3>Collections</h3>
+                    <h3>Deleted</h3>
+
                 </div>
-                <Routes>
-                    <Route 
-                        path="/" 
-                        element={<GalleryPage />} 
-                        />
-                    <Route 
-                        path="/upload" 
-                        element={<UploadPage />} 
-                        />
-               </Routes>
-            </BrowserRouter>
+
+                <div id="content" className={this.state.sidebarOpen ? "open" : ""} >
+                    <BrowserRouter>
+                            <div className="nav">
+                                <button 
+                                    className="menu" 
+                                    onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}
+                                    >
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </button>
+                            <div className="title">Photosphere</div>
+                            <Link to="/" className="link">Home</Link>
+                            <Link to="/upload" className="link">Upload</Link>
+                        </div>
+                        <Routes>
+                            <Route 
+                                path="/" 
+                                element={<GalleryPage />} 
+                                />
+                            <Route 
+                                path="/upload" 
+                                element={<UploadPage />} 
+                                />
+                    </Routes>
+                    </BrowserRouter>
+                </div>
+            </div>
         );
     }
 }
