b2ade158066b25c914c60e30497e3f01e387899c
Ashley Davis <ashley@codecapers.com.au>
2022-12-31 10:55:22 +1000

    Installed Font Awesome.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from "./app";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
