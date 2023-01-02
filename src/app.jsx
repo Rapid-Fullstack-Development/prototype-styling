67d0cc2e6b515a57fb25e6fb24baa3649af976d3
Ashley Davis <ashley@codecapers.com.au>
2023-01-02 11:20:16 +1000

    Removed the "host" component. It doesn't seem to be necessary to maintain the sidebar/navbar structures.

--- src/app.jsx
+++ src/app.jsx
@@ -16,106 +16,102 @@ export class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div className="host">
-                    <div id="navbar" >
-                        <div className="flex flex-row items-center pl-4 pt-2 pb-2">
-                            <button 
-                                onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}
-                                >
-                                <i className="fa-solid fa-bars"></i>
-                            </button>
+                <div id="navbar" >
+                    <div className="flex flex-row items-center pl-4 pt-2 pb-2">
+                        <button 
+                            onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}
+                            >
+                            <i className="fa-solid fa-bars"></i>
+                        </button>
 
-                            <h1 className="text-xl ml-3">Photosphere</h1>
+                        <h1 className="text-xl ml-4">Photosphere</h1>
 
-                            <div className="flex-grow" />
+                        <Link to="/" className="ml-auto mr-4 sm:mr-6">
+                            <div className="flex flex-row items-center text-gray-700">
+                                <i className="w-6 text-center fa-solid fa-cloud"></i>
+                                <div className="hidden sm:block ml-2">Storage</div>
+                            </div>
+                        </Link>
 
-                            <Link to="/" className="mr-4 sm:mr-6">
-                                <div className="flex flex-row items-center text-gray-700">
-                                    <i className="w-6 text-center fa-solid fa-cloud"></i>
-                                    <div className="hidden sm:block ml-2">Storage</div>
-                                </div>
-                            </Link>
-
-                            <Link to="/" className="mr-4 sm:mr-6">
-                                <div className="flex flex-row items-center text-gray-700">
-                                    <i className="w-6 text-center fa-solid fa-hard-drive"></i>
-                                    <div className="hidden sm:block ml-2">Local</div>
-                                </div>
-                            </Link>
+                        <Link to="/" className="mr-4 sm:mr-6">
+                            <div className="flex flex-row items-center text-gray-700">
+                                <i className="w-6 text-center fa-solid fa-hard-drive"></i>
+                                <div className="hidden sm:block ml-2">Local</div>
+                            </div>
+                        </Link>
 
                             <Link to="/upload" className="mr-8">
-                                <div className="flex flex-row items-center text-gray-700">
-                                    <i className="w-6 text-center fa-solid fa-upload"></i>
-                                    <div className="hidden sm:block ml-2">Upload</div>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div id="sidebar" className={this.state.sidebarOpen ? "open" : ""} >
-                        <div className="flex flex-row items-center mt-2 mb-8">
-                            <h1 className="text-xl">
-                                Photosphere
-                            </h1>
-                            <div className="flex-grow" />
-                            <button 
-                                className="mr-3 text-xl" 
-                                onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}
-                                >
-                                <i className="fa-solid fa-arrow-left"></i>
-                            </button>
-                        </div>
-
-                        <Link to="/" className="">
-                            <div className="flex flex-row items-center pl-1 text-gray-700">
-                                <i className="w-12 text-center fa-solid fa-cloud"></i>
-                                <div className="">Storage</div>
+                            <div className="flex flex-row items-center text-gray-700">
+                                <i className="w-6 text-center fa-solid fa-upload"></i>
+                                <div className="hidden sm:block ml-2">Upload</div>
                             </div>
                         </Link>
+                    </div>
+                </div>
 
-                        <Link to="/" className="">
-                            <div className="flex flex-row items-center pl-1 mt-2 text-gray-700">
-                                <i className="w-12 text-center fa-solid fa-hard-drive"></i>
-                                <div className="">Local</div>
-                            </div>
-                        </Link>
-
-                        <Link to="/upload" className="">
-                            <div className="flex flex-row items-center pl-1 mt-2 text-gray-700">
-                                <i className="w-12 text-center fa-solid fa-upload"></i>
-                                <div className="">Upload</div>
-                            </div>
-                        </Link>
-
-                        <div className="flex flex-row items-center pl-1 mt-8 text-gray-700 cursor-pointer">
-                            <i className="w-12 text-center fa-regular fa-star"></i>
-                            <div className="">Favorites</div>
-                        </div>
-
-                        <div className="flex flex-row items-center pl-1 mt-2 text-gray-700 cursor-pointer">
-                            <i className="w-12 text-center fa-regular fa-images"></i>
-                            <div className="">Collections</div>
-                        </div>
-
-                        <div className="flex flex-row items-center pl-1 mt-2 text-gray-700 cursor-pointer">
-                            <i className="w-12 text-center fa-regular fa-trash-can"></i>
-                            <div className="">Deleted</div>
-                        </div>
-
+                <div id="sidebar" className={this.state.sidebarOpen ? "open" : ""} >
+                    <div className="flex flex-row items-center mt-2 mb-8">
+                        <h1 className="text-xl">
+                            Photosphere
+                        </h1>
+                        <div className="flex-grow" />
+                        <button 
+                            className="mr-3 text-xl" 
+                            onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}
+                            >
+                            <i className="fa-solid fa-arrow-left"></i>
+                        </button>
                     </div>
 
-                    <div id="content" className={this.state.sidebarOpen ? "open" : ""} >
-                            <Routes>
-                                <Route 
-                                    path="/" 
-                                    element={<GalleryPage />} 
-                                    />
-                                <Route 
-                                    path="/upload" 
-                                    element={<UploadPage />} 
-                                    />
-                        </Routes>
+                    <Link to="/" className="">
+                        <div className="flex flex-row items-center pl-1 text-gray-700">
+                            <i className="w-12 text-center fa-solid fa-cloud"></i>
+                            <div className="">Storage</div>
+                        </div>
+                    </Link>
+
+                    <Link to="/" className="">
+                        <div className="flex flex-row items-center pl-1 mt-2 text-gray-700">
+                            <i className="w-12 text-center fa-solid fa-hard-drive"></i>
+                            <div className="">Local</div>
+                        </div>
+                    </Link>
+
+                    <Link to="/upload" className="">
+                        <div className="flex flex-row items-center pl-1 mt-2 text-gray-700">
+                            <i className="w-12 text-center fa-solid fa-upload"></i>
+                            <div className="">Upload</div>
+                        </div>
+                    </Link>
+
+                    <div className="flex flex-row items-center pl-1 mt-8 text-gray-700 cursor-pointer">
+                        <i className="w-12 text-center fa-regular fa-star"></i>
+                        <div className="">Favorites</div>
                     </div>
+
+                    <div className="flex flex-row items-center pl-1 mt-2 text-gray-700 cursor-pointer">
+                        <i className="w-12 text-center fa-regular fa-images"></i>
+                        <div className="">Collections</div>
+                    </div>
+
+                    <div className="flex flex-row items-center pl-1 mt-2 text-gray-700 cursor-pointer">
+                        <i className="w-12 text-center fa-regular fa-trash-can"></i>
+                        <div className="">Deleted</div>
+                    </div>
+
+                </div>
+
+                <div id="content" className={this.state.sidebarOpen ? "open" : ""} >
+                        <Routes>
+                            <Route 
+                                path="/" 
+                                element={<GalleryPage />} 
+                                />
+                            <Route 
+                                path="/upload" 
+                                element={<UploadPage />} 
+                                />
+                    </Routes>
                 </div>
             </BrowserRouter>
         );
